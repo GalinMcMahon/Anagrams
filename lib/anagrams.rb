b3c787897a06b5a17d52aa5038d961e8defcfcbe
Galin McMahon <galinmcmahon@gmail.com>
2017-04-21 16:26:59 -0700

    save current progress between third and fourth passing specs

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,27 +1,51 @@
 class String
+
+
+            # THIS COMMENTED OUT SECTION ISNT WORKING
+            # AFTER SEVERAL REWRITES
+            # WILL RESUBMIT
+
+  # define_method(:vowel_check?) do
+  #   vowels = ["a", "e", "i", "o", "u"]
+  #   # self.each do |vowel|
+  #   included = self.split("")
+  #   includes_vowels = included.each_with_index() do |letter,index|
+  #     included.include?(vowels)
+  #     if includes_vowels === true
+  #       vowel_output = "Good job"
+  #     else
+  #       false
+  #       vowel_output = "You need to use real words."
+  #     end
+  #   end
+  #   vowel_output
+  #   # end # ends if statement
+  # end
+
+
   define_method(:anagrams) do |word2|
     word1 = self.downcase.chars.sort.join
     word2 = word2.downcase.chars.sort.join
-  if word1 == word2
-    returned_output = "These words are anagrams!"
-  else
-    returned_output = "These aren't anagrams."
-  end
-  returned_output
-end
-end
 
-
+    if word1 == word2
+      returned_output = "These words are anagrams!"
+    else
+      returned_output = "These aren't anagrams."
+    end # ends if statement
+    returned_output
+  end # ends anagrams method
+end # ends string anagrams
 
 class String
   define_method(:palindromes) do
     pal_word1 = self
     pal_word_reversed = self.reverse()
-  if pal_word1 == pal_word_reversed
-    palindrome_output = "Your word is a palindrome"
-  else
-    palindrome_output = "Your word is not a palindrome"
-  end
-  palindrome_output
-  end
-end
+    if pal_word1 == pal_word_reversed
+      palindrome_output = "Your word is a palindrome"
+    else
+      palindrome_output = "Your word is not a palindrome"
+    end # ends if statement
+    palindrome_output
+
+  end # ends palindromes method
+end # ends class string
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -1,6 +1,7 @@
 require('rspec')
 require('./lib/anagrams')
 
+
 describe('String#anagrams') do
   it("returns true or false based on whether the words are anagrams or not") do
     expect("stop".anagrams("spot")).to(eq("These words are anagrams!"))
@@ -11,9 +12,14 @@ describe('String#anagrams') do
   end
 end
 
+describe('String#vowel_check') do
+  it("checks words to make sure a vowel exists") do
+    expect("stop".vowel_check?("tops")).to(eq("Good job"))
 
+  end
+end
 
-  describe('String#palindromes') do
+describe('String#palindromes') do
   it("returns a true or false statement based on whether the input word is a palindrome") do
     expect(("radar").palindromes()).to(eq("Your word is a palindrome"))
   end
